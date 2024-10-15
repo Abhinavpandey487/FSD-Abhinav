07e60a432da12dfd0a566fe00957610b7a32a239
Abhinavpandey487 <abhinavpandey010203@gmail.com>
2024-10-15 15:35:30 +0530

    commit js

---
+++ day5/script.js
@@ -0,0 +1,16 @@
+// let x=confirm("Are you sure to log out");
+// alert(x);
+
+// let v=prompt("Enter your name","Abhinav");
+// alert(v);
+// let disp=()=>{
+//     console.log("Welcome to arrow function");
+// }
+// disp();
+let dis=()=>console.log("welcome to arrow function")
+dis()
+let sum=(x,y)=>{
+    return x+y
+}
+let res=sum(3,4)
+console.log(res)
